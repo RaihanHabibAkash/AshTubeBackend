14e770fa25ce18e79f4cc12e72b708afc0725be4
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-01-27 12:19:09 +0600

    Formated

--- src/db/mongoCon.js
+++ src/db/mongoCon.js
@@ -7,11 +7,15 @@ const mongoDbUri = process.env.MONGODB_URI;
 const dbName = "AshTube";
 
 export const connectedDB = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${mongoDbUri}/${dbName}`);
-        console.log(`\n MongoDB is connected! Cennection host: ${connectionInstance.connection.host}`);
-    } catch (error) {
-        console.log("Error while Connecting MongoDB: ", error);
-        process.exit(1);
-    }
-} 
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${mongoDbUri}/${dbName}`
+    );
+    console.log(
+      `\n MongoDB is connected! Cennection host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("Error while Connecting MongoDB: ", error);
+    process.exit(1);
+  }
+};
