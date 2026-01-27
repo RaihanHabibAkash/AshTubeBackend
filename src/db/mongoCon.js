8f50986bd124be4e9de8a83796bc9435bbf693da
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-01-27 12:10:38 +0600

    MongoDB Connected.

---
+++ src/db/mongoCon.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const mongoDbUri = process.env.MONGODB_URI;
+const dbName = "AshTube";
+
+export const connectedDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${mongoDbUri}/${dbName}`);
+        console.log(`\n MongoDB is connected! Cennection host: ${connectionInstance.connection.host}`);
+    } catch (error) {
+        console.log("Error while Connecting MongoDB: ", error);
+        process.exit(1);
+    }
+} 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import { connectedDB } from "./db/mongoCon.js";
 
 dotenv.config({ path: "../.env" });
 const app = express();
@@ -7,4 +8,5 @@ const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port: ${port}`);
+  connectedDB();
 });
