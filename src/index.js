bbac0c4375769742da166d6f1e32ef5bc78de322
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-01-27 11:38:21 +0600

    Format error done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('Hlw');
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config({ path: "../.env" });
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port: ${port}`);
+});
