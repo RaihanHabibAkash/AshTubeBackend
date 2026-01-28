9e21de3933ea51ffc0c9e2a4db9214fcd24ef363
Raihan Habib Akash <raihanakash2003811@gmail.com>
2026-01-28 15:36:25 +0600

    Mongodb Connection logic updated and Express error handeling using app.on()

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ dotenv.config({ path: "../.env" });
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-  console.log(`Example app listening on port: ${port}`);
-  connectedDB();
+app.on("error", (error) => {
+  console.log("Error while starting the server", error);
+  process.exit(1);
+});
+
+connectedDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Example app listening on port: ${port}`);
+  });
+}).catch((error) => {
+  console.log("Error while connectingMongoDB:", error);
 });
